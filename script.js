5b053c2d59ddd5b92c510edeed84ad070d447896
dltkdgus482 <dltkdgus482@naver.com>
2025-01-12 01:17:27 +0900

    feat: 3초 동안 블러 제거 기능 추가

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const init = (removeAdditionalBlur, removeAllBlur) => {
+const init = (addBlur, removeAdditionalBlur, removeAllBlur) => {
+  const addBlurButton = document.getElementById("add-blur-button");
   const removeAdditionalBlurButton = document.getElementById(
     "remove-additional-blur-button"
   );
   const removeAllBlurButton = document.getElementById("remove-all-blur-button");
 
+  addBlurButton.onclick = addBlur;
   removeAdditionalBlurButton.onclick = removeAdditionalBlur;
   removeAllBlurButton.onclick = removeAllBlur;
 };
@@ -40,27 +42,74 @@ const initBlur = () => {
   initBlurWordSet.forEach((word) => blur(word));
 };
 
+const addBlur = () => {
+  console.log("addBlur");
+};
+
 const removeAdditionalBlur = () => {
   console.log("removeAdditionalBlur");
 };
 
 const removeAllBlur = () => {
-  console.log("removeAllBlur");
+  const content = document.getElementById("content");
+
+  if (!content) {
+    console.log("id가 content인 요소가 존재하지 않습니다.");
+    return;
+  }
+
+  const q = [];
+  q.push({ el: content, depth: 0 });
+
+  startCountdown();
+
+  while (q.length > 0) {
+    const { el, depth } = q.shift();
+
+    const classList = el.classList;
+
+    if (classList.contains("blurred")) {
+      classList.remove("blurred");
+
+      setTimeout(() => classList.add("blurred"), 3000);
+    }
+
+    Array.from(el.children).forEach((child) => {
+      q.push({ el: child, depth: depth + 1 });
+    });
+  }
+};
+
+const startCountdown = () => {
+  const countdown = document.getElementById("remove-all-blur-button");
+
+  let count = 3;
+  countdown.innerText = count;
+
+  const interval = setInterval(() => {
+    count -= 1;
+    countdown.innerText = count;
+
+    if (count === 0) {
+      clearInterval(interval);
+      countdown.innerText = "원본 보기";
+    }
+  }, 1000);
 };
 
 const blur = (word) => {
   const content = document.getElementById("content");
 
-  Array.from(content.children).forEach((element) => {
-    const originalText = element.textContent;
+  Array.from(content.children).forEach((e) => {
+    const originalText = e.textContent;
     const updatedText = originalText.replaceAll(
       word,
       `<span class="blurred">${word}</span>`
     );
 
-    element.innerHTML = updatedText;
+    e.innerHTML = updatedText;
   });
 };
 
-init(removeAdditionalBlur, removeAllBlur);
+init(addBlur, removeAdditionalBlur, removeAllBlur);
 initBlur();
